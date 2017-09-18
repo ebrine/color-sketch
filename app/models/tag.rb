9d50511f58e770ea50051ee69fb12072a4947aa8
John Simoni & Miguel Angel Ibarra, Jr <pair+jsimoni42.migaibarra@devbootcamp.com>
2017-09-18 15:34:46 -0700

    Wrote more tests for tag, problems with test database not cleaning itself

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,5 @@
 class Tag < ApplicationRecord
   has_many :joins
   has_many :entries, through: :joins
+  validates :tag_name, presence: true
 end
--- spec/tag_spec.rb
+++ spec/tag_spec.rb
@@ -1,7 +1,18 @@
 require 'spec_helper'
 
 describe Tag do
-  pending "it can have many entries"
-  pending "it must have at least one entry"
-  pending "it must have a title"
+  let (:tag) {Tag.create({tag_name: ["1"]})}
+  it "it can have many entries" do
+
+  end
+  it "name cannot be blank" do
+    t = Tag.new({tag_name: nil})
+    expect(t).to_not be_valid
+  end
+  it "it must have a title"
+  it "tags must be at least one" do
+    Tag.create({tag_name: "Cher"})
+    expect(Tag.count > 0).to eq true
+    expect(Tag.all.pluck(:tag_name)).to include("Cher")
+  end
 end
