d0e7f464296dabcd63541808057eea59b7bcd19b
Blaine Anderson & Jamie D <pair+blainea97.seajamied@devbootcamp.com>
2017-09-18 18:03:37 -0700

    uploading for new computer

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,12 @@ get '/' do
   @entries = Blog.find(1).entries
   erb :index
 end
+
+get '/new_entry' do
+  erb :new_entry
+end
+
+post '/new_entry' do
+  Entry.create(blog_id: 1, title: params[:title], article: params[:article])
+  redirect '/'
+end
