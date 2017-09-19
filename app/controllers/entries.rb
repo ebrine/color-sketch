f0ca25ae65cb9e913fe63e7df13c0f6a93a983f4
John Simoni & Miguel Angel Ibarra, Jr <pair+jsimoni42.migaibarra@devbootcamp.com>
2017-09-18 18:47:31 -0700

    created an entry edit page

--- app/controllers/entries.rb
+++ app/controllers/entries.rb
@@ -7,3 +7,10 @@ get '/entry/:id' do
   @current_entry = Entry.find(params[:id])
   erb :entry
 end
+
+post '/entry/:id' do
+  entry = Entry.find(params[:id])
+  entry.update_attributes({title: params[:title], body: params[:body],
+    updated_at: DateTime.now})
+  redirect to("/entry/#{entry.id}")
+end
