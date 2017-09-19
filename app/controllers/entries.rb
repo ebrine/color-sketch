c080316782feaa022a9e1dacf5976386215d8d83
Marlo Ito & Piper Denney <pair+marloito.piperdenney@devbootcamp.com>
2017-09-18 19:20:22 -0700

    links homepage to appropriate post

--- app/controllers/entries.rb
+++ app/controllers/entries.rb
@@ -1,3 +1,4 @@
-get '/entry' do
+get '/entry/:id' do
+  @entry = Entry.find(params[:id])
   erb :entries
 end
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,4 +1,6 @@
 class Entry < ApplicationRecord
   has_and_belongs_to_many :tags
   validates :title, :description, presence: true
+
+  
 end
