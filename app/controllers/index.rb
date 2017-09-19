0e6f1dedc3e7e643e2b1ca6dc6eab756623f7d5c
John Simoni <email": null>
2017-09-19 08:25:56 -0700

    Error logging for new posts

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,16 +1,29 @@
+@errors = []
+enable :sessions
+
 get '/' do
   @posts = Entry.all
+  if session[:errors].length != 0
+    @errors = session[:errors]
+  end
   erb :index
 end
 
 post '/' do
-  tag_name = params[:tag]
-  if tag_name =~ /\W/
-    redirect to("/")
+  session[:errors] = []
+  tag_name = params[:tag].chomp
+  @posts = Entry.all
+  if tag_name.match(/[\w\s]*/)[0] != tag_name
+    session[:errors] << "Tag name must only include alphabetic characters."
   end
   tag = Tag.create({tag_name: tag_name})
-  Entry.create({title: params[:title], body: params[:body], tags: [tag]})
-  redirect to("/")
+  session[:errors] << tag.errors.full_messages
+  e = Entry.create({title: params[:title], body: params[:body], tags: [tag]})
+  session[:errors] << e.errors.full_messages
+  session[:errors] = session[:errors].flatten
+  puts "****************************************"
+  puts "#{session[:errors]}"
+  redirect to ("/")
 end
 
 post "/search" do
