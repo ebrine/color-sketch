1e138619943a8b6301e2d19bc8ef1d7ce1763426
John Simoni & Miguel Angel Ibarra, Jr <pair+jsimoni42.migaibarra@devbootcamp.com>
2017-09-18 17:00:19 -0700

    Changed index page to accept new journal entries and display them

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,12 @@
+# @posts = [{"title" => "Cher", "body" => "Is a wonderful artist.", "tags" => ["Artist", "Wonderful", "Wow"]}]
+
 get '/' do
-  # Look in app/views/index.erb
+  @posts = Entry.all
   erb :index
+end
+
+post '/' do
+  tag = Tag.create({tag_name: params[:tag]})
+  Entry.create({title: params[:title], body: params[:body], tags: [tag]})
+  redirect to("/")
 end
