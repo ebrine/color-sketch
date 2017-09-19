f1dc00776c41eb53387323bd4aa898d9392bcf20
Jordan Kamin <jordanakamin@gmail.com>
2017-09-19 10:29:39 -0700

    adds files for model specs

--- db/migrate/20170919094629_create_entries_tags.rb
+++ db/migrate/20170919094629_create_entries_tags.rb
@@ -1,8 +1,8 @@
 class CreateEntriesTags < ActiveRecord::Migration[5.0]
   def change
     create_table :entries_tags do |blaine|
-      blaine.belongs_to :entry
-      blaine.belongs_to :tag
+      blaine.belongs_to :entry, index: true
+      blaine.belongs_to :tag, index: true
       blaine.timestamps null: false
     end
   end
---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+100.times do
+  Entry.create(title: 'Ham', body: 'Hella Hams', author: 'Hammstein')
+end
