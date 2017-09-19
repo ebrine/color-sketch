ad9d9bdba29fe33f64aa048340fa5c73e53defe0
Jamie Degnan <jamie.degnan@gmail.com>
2017-09-19 08:40:27 -0700

    Add support for a single tag

---
+++ app/controllers/entries.rb
@@ -0,0 +1,43 @@
+get '/:id' do
+    article_to_find = params[:id]
+    @current_id = article_to_find.to_i
+    @article = Post.find_by(id: article_to_find.to_i)
+  #need to create a 404 page
+  erb :index
+end
+
+get '/new_entry/' do
+  erb :new_entry
+end
+
+post '/new_entry' do
+  title = params[:title]
+  article = params[:article]
+  tag = params[:tags]
+  post = Post.create(title: title, article: article)
+  post.tags << Tag.create(tag: tag)
+  redirect '/'
+end
+
+get '/:id/edit_entry' do
+  article_to_find = params[:id]
+  @current_id = article_to_find.to_i
+  @article = Post.find_by(id: article_to_find.to_i)
+  erb :edit_entry
+end
+
+post '/:id/edit_entry' do
+  id = params[:id]
+  post_to_update = Post.find(id)
+  title = params[:title]
+  article = params[:article]
+  post_to_update.update(title: title, article: article)
+  redirect "/#{id}"
+end
+
+get '/:id/delete_entry' do
+  id = params[:id]
+  @article = Post.find(id)
+  Post.find(id).destroy
+  erb :delete_entry
+end
