746381648a09187719544e14e9a8ac919526f3bb
Jordan Kamin <jordanakamin@gmail.com>
2017-09-19 09:43:58 -0700

    fills out entries migration

--- db/migrate/20170919093706_create_entries.rb
+++ db/migrate/20170919093706_create_entries.rb
@@ -1,4 +1,10 @@
 class CreateEntries < ActiveRecord::Migration[5.0]
   def change
+    create_table :entries do |t|
+      t.string :title
+      t.text :body
+      t.string :author
+      t.timestamps(null: false)
+    end
   end
 end
