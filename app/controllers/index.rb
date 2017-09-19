2a143ad6be4372f8003bf8576e72e3b01e133684
Blaine Anderson <email": null>
2017-09-18 20:19:16 -0700

    unfinished edit and delete functionality

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -11,3 +11,12 @@ post '/new_entry' do
   Entry.create(blog_id: 1, title: params[:title], article: params[:article])
   redirect '/'
 end
+
+get '/single_entry/:id' do
+  @for_now = Entry.find_by(id: params[:id])
+  erb :single_entry
+end
+
+post '/update_title' do
+
+end
