e062901eb5b7a97ca9dd548cc713897dacdae249
nick-vanderen & Eliza Brine <pair+Nickvanderen.ebrine@devbootcamp.com>
2017-09-18 20:40:08 -0700

    Implements edit and delete entries including tags

--- app/controllers/entries.rb
+++ app/controllers/entries.rb
@@ -15,15 +15,82 @@ get '/entry/' do
   erb :entry
 end
 
-post '/entry' do
-  # create a new entry
-  # tag the entry with relevant tags
+get '/publish' do
+  @tag_names = Tag.all.map do |tag|
+    tag.name
+  end
+  erb :entry_form
 end
 
-patch '/edit' do
+post '/publish' do
+  title = params[:title]
+  author = params[:author]
+  body = params[:body]
+  new_entry = Entry.create(title: title, author: author, body: body)
+
+  @tag_names = Tag.all.map do |tag|
+    tag.name
+  end
+
+  @tag_names.each do |name|
+    if params[name.to_sym] == "on"
+      new_entry.tags << Tag.all.find_by(name: name)
+    end
+  end
+
+  if params[:other]
+    new_entry.tags << Tag.create(name: params[:other])
+  end
+
+  redirect '/'
+end
+
+post '/edit' do
   # updates the entry
+  @tag_names = Tag.all.map do |tag|
+    tag.name
+  end
+  @entry_id = params[:entry_id]
+  entry = Entry.find(@entry_id)
+  @entry_title = entry.title
+  @entry_author = entry.author
+  @entry_body = entry.body
+  @entry_tag_names = entry.tags.map { |tag| tag.name }
+  @other_tags = @tag_names - @entry_tag_names
+  erb :edit_form
 end
 
-patch '/delete' do
-  # deletes the selected entry
+post '/update' do
+  entry = Entry.find(params[:entry_id])
+  entry.title = params[:title]
+  entry.author = params[:author]
+  entry.body = params[:body]
+
+  @tag_names = Tag.all.map do |tag|
+    tag.name
+  end
+
+  entry.tags = []
+
+  if @tag_names
+    @tag_names.each do |name|
+      if (params[name.to_sym] == "on")
+        entry.tags << Tag.all.find_by(name: name)
+      end
+    end
+  end
+
+  if (params[:other_tag_entry] != "") && (@tag_names.include?params[:other_tag_entry] ==false)
+    entry.tags << Tag.create(name: params[:other_tag_entry])
+  end
+  
+  entry.save
+  redirect '/'
+end
+
+post '/delete' do
+  @entry_id = params[:entry_id]
+  entry_to_delete = Entry.find(@entry_id)
+  entry_to_delete.destroy
+  erb :deleted_form
 end
