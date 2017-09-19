87600b36a555e7bb51e45147b680a8ed42a1204c
Jamie Degnan <jamie.degnan@gmail.com>
2017-09-18 21:23:50 -0700

    provide option to delete

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,5 +1,5 @@
 get '/' do
-  @posts = Post.all
+  @posts = Post.all.sort_by &:created_at
   erb :index
 end
 
@@ -26,4 +26,20 @@ get '/:id/edit_entry' do
   @current_id = article_to_find.to_i
   @article = Post.find_by(id: article_to_find.to_i)
   erb :edit_entry
+end
+
+post '/:id/edit_entry' do
+  id = params[:id]
+  post_to_update = Post.find(id)
+  title = params[:title]
+  article = params[:article]
+  post_to_update.update(title: title, article: article)
+  redirect "/#{id}"
+end
+
+get '/:id/delete_entry' do
+  id = params[:id]
+  @article = Post.find(id)
+  Post.find(id).destroy
+  erb :delete_entry
 end
