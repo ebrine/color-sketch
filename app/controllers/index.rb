b9528ead254aae8cccdb171fe573998b3b0be529
Amanuel <yihdego@msn.com>
2017-09-20 17:02:17 -0700

    blog has a button that sends you to create new blog and homebutton on show

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,12 +3,16 @@ get '/' do
   erb :index
 end
 
-# post '/entries' do
-#   blog = Entry.create(title: params[:title], body: params[:body])
-#   blog.tags << Tag.find_or_create_by(description: params[:tag])
-#   redirect '/'
-#
-# end
+get '/entries/new' do
+  erb :new
+end
+
+post '/entries' do
+  blog = Entry.create(title: params[:title], body: params[:body])
+  blog.tags << Tag.find_or_create_by(description: params[:tag])
+  redirect '/'
+
+end
 
 get '/entries/:id' do
   @entry = Entry.find(params[:id])
