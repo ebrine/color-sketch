d01a1b046bbfd671cbb4b6bdaceb821747f8c69e
Amanuel <yihdego@msn.com>
2017-09-20 17:42:05 -0700

    working on edit, so far able to click the edit button

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -11,10 +11,28 @@ post '/entries' do
   blog = Entry.create(title: params[:title], body: params[:body])
   blog.tags << Tag.find_or_create_by(description: params[:tag])
   redirect '/'
-
 end
 
 get '/entries/:id' do
   @entry = Entry.find(params[:id])
   erb :show
+end
+
+get '/entries/:id/edit' do
+  @entry = Entry.find(params[:id])
+  erb :edit
+end
+
+def edit_entry
+  blog = Entry.find(params[:id])
+  blog.body = params[:body]
+  redirect "/entries/#{blog.id}"
+end
+
+patch '/entries/:id' do
+  edit_entry
+end
+
+put '/entries/:id' do
+  edit_entry
 end
