c575f9dfa5f4c3554a942e83a9e4d3aee2029c3c
Miguel Angel Ibarra, Jr <miggs.ibarra@gmail.com>
2017-10-02 15:55:01 -0700

    the new page /entries/new returns a 200 status code

--- app/controllers/entries.rb
+++ app/controllers/entries.rb
@@ -2,3 +2,7 @@ get '/entries' do
   @entries = Entry.all
   erb :'entries/index'
 end
+
+get '/entries/new' do
+  erb :'entries/new'
+end
--- spec/controllers/entries_controller_spec.rb
+++ spec/controllers/entries_controller_spec.rb
@@ -1,12 +1,18 @@
 require_relative '../spec_helper'
 
 describe "Entries Controller" do
+  let(:test_entry) { Entry.create(
+      author: "Anonymous Ace",
+      title: "There be dragons here!",
+      body: "I like training my dragon"
+    )}
+
   it '/entries returns a 200 status code' do
     get '/entries'
     expect(last_response.status).to eq(200)
   end
 
-  pending 'entries/new returns a 200 status code' do
+  it 'entries/new returns a 200 status code' do
     get '/entries/new'
     expect(last_response.status).to eq(200)
   end
