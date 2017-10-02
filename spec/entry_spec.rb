67c9d2eb154494bd50febb3bda70368e522394ea
Miguel Angel Ibarra, Jr <miggs.ibarra@gmail.com>
2017-10-02 12:22:56 -0700

    creates tests for the Entry model.

--- spec/entry_spec.rb
+++ spec/entry_spec.rb
@@ -1,5 +1,21 @@
 require 'spec_helper'
 
 describe Entry do
-  pending "add some examples to (or delete) /Users/apprentice/Desktop/blog-1-anonymous-blog-challenge/Rakefile"
+  let(:entry_sample) {Entry.new(
+    title: "Something Good",
+    body: "Lorem ipsum whatever")}
+
+  pending "is not valid without a title" do
+    entry_sample.title = ""
+    expect(entry_sample).to_not be_valid
+  end
+
+  pending "is not valid without a body" do
+    entry_sample.body = ""
+    expect(entry_sample).to_not be_valid
+  end
+
+  pending "is valid with both a title and a body" do
+    expect(entry_sample).to be_valid
+  end
 end
