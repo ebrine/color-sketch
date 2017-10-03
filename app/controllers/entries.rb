62a12d5120bc0a8eb4eda29d30ab25175dae5b47
Marlo Ito <email": null>
2017-10-02 20:54:10 -0700

    Lines 46-53 are broken. Come back tomorrow.

--- app/controllers/entries.rb
+++ app/controllers/entries.rb
@@ -30,6 +30,27 @@ end
 put '/entries/:id' do
   @entry = Entry.find(params[:id])
   @entry.update_attributes(title: params[:title], description: params[:description])
+  tags = params[:tags].split(",")
+  p "-" *300
+  p tags
+  p "-" *300
+  tags.each do |name|
+    @entry.tags.each do |tag|
+      if tag.name != name
+        new_tag = Tag.find_or_create_by(name: name)
+        EntriesTag.find_or_create_by(entry: @entry, tag: new_tag)
+      end
+    end
+  end
+
+  @entry.tags.each do |tag|
+    name = tag.name
+    p name
+    if tags.exclude?(name)
+    association = EntriesTag.find_by(entry: @entry, tag: tag)
+    association.destroy
+    end
+  end
   redirect "/entries/#{@entry.id}"
 end
 
