72468d7102cf9149976b749783718ca836bb332c
nick-vanderen <email": null>
2017-10-04 14:31:37 -0700

    new entry route, new entry placeholder page

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,6 +2,14 @@ get '/' do
   redirect "/entries"
 end
 
+# index -->
 get '/entries' do
+  @entries = Entry.all
   erb :'entries/index'
+end
+
+# new -->
+
+get '/entries/new' do
+  erb:'entries/new'
 end
