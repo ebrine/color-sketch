546b448f70817702d55213ad5ec1dfd92970b2a5
Eliza Brine <email": null>
2017-10-04 16:36:18 -0700

    Creates view with drawing pane and palette. Adds JS to implement palette painting

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,31 @@
-$(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+const colors = {
+  p1: "indigo",
+  p2: "olivedrab",
+  p3: "powderblue",
+  p4: "salmon",
+  p5: "peru",
+  p6: "mediumvioletred"
+}
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+
+class Paintbrush {
+  constructor() {
+    this.color = 'lightgrey';
+  }
+  paint(cell) {
+    $(cell).css('background-color', this.color);
+    console.log("HI");
+  }
+};
+
+$(document).ready(function() {
+  brush  = new Paintbrush();
+  $('.paint').on('click', (e) => {
+    let id = $(e.currentTarget).attr('id');
+    let selectedColor = colors[id];
+    brush.color = selectedColor;
+  })
+  $(".cell").on('mouseover', (e) => {
+    brush.paint(e.currentTarget);
+  })
 });
