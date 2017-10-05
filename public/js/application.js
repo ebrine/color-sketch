50f82ee7be52010a5832716cd05e8f8d311b3982
Eliza Brine <email": null>
2017-10-04 18:14:14 -0700

    Adds buttons for templates

--- public/js/application.js
+++ public/js/application.js
@@ -10,7 +10,7 @@ const colors = {
 
 class Paintbrush {
   constructor() {
-    this.color = 'lightgrey';
+    this.color = 'floralwhite';
   }
   paint(cell) {
     $(cell).css('background-color', this.color);
@@ -18,14 +18,51 @@ class Paintbrush {
   }
 };
 
+createPatternGrid = (pattern) => {
+  console.log(pattern.length);
+  // "<div class="cell">
+  // </div>"
+}
+
+class Pattern {
+  constructor(object){
+    this.names = Object.keys(object);
+  }
+
+}
+
 $(document).ready(function() {
   brush  = new Paintbrush();
   $('.paint').on('click', (e) => {
     let id = $(e.currentTarget).attr('id');
     let selectedColor = colors[id];
     brush.color = selectedColor;
-  })
+  });
+
   $(".cell").on('mouseover', (e) => {
     brush.paint(e.currentTarget);
+  });
+
+  $('.template-button').one('click', (e) => {
+    $.ajax({
+      method: "GET",
+      url: '/templates'
+    }).done((response) => {
+    const patterns = new Pattern(response);
+    patterns.names.forEach((name) => {
+      let newButton = $(`<button class="single-template-button" value="${name}">${name}</button>`);
+      newButton.appendTo($('.template-loader'));
+    })
+  })
+  })
+
+  $('.template-loader').on('click', '.single-template-button', (e) => {
+    const val = $(e.currentTarget).val()
+    $.ajax({
+      method: "GET",
+      url: `/templates/${val}`
+    }).done((response) => {
+      createPatternGrid(response);
+    })
   })
 });
